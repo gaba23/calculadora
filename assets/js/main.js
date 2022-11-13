faf642fc462870588d970aba1705f5e9dc924f7a
Gabriel Alves <112426768+gaba23@users.noreply.github.com>
2022-11-13 20:10:31 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ function criaCalculadora (){
       try {
         conta = eval(conta);
         
-        if(!conta){
+        if(!conta && conta!==0){
           alert('Conta inválida');
           return;
         }
